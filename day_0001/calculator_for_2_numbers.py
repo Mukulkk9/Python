c9cb765760007c8170de1e94e689812f343e79dc
Mukul <mukulkhandalkar4@gmail.com>
2025-10-10 12:53:31 +0530

    Simple calculator project

---
+++ day_0001/calculator_for_2_numbers.py
@@ -0,0 +1,10 @@
+try:
+	numbers=list(map(float,input("Enter Two Numbers:").split()))
+	operation=input("Select The Operator(+,-,*,/,%,//,**):")
+
+	if len(numbers)!=2:
+	print("Please do not enter more than two numbers!!")
+
+	else:
+	match operation:
+	
