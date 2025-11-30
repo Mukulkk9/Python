2bd5b25e15a8436f157bfe262af3b50abe51b65f
Mukul <mukulkhandalkar4@gmail.com>
2025-11-30 14:08:14 +0530

    simple calculator for 2 numbers using match case

--- day_2/calculator_for_2_numbers.py
+++ day_2/calculator_for_2_numbers.py
@@ -1,9 +1,38 @@
-try:
-	numbers=list(map(float,input("Enter Two Numbers:").split()))
-	operation=input("Select The Operator(+,-,*,/,%,//,**):")
+while True:
+ try:
+  num=list(map(float,input("Enter Two Numbers:").split()))
+  print(num)
 
-	if len(numbers)!=2:
-	print("Please do not enter more than two numbers!!")
+  if len(num)!=2:
+   print("Please do not enter more than two numbers!!")
+   raise ValueError
+  break
+ except ValueError:
+  print("TrY AgAiN...!")
 
-	else:
-	match operation:
+if(len(num)==2):
+ operation=input("Select The Operator(+,-,*,/,%,//,**):")
+ match operation:
+  case '+':
+   print(f"The Addition of Two Numbers is:{num[0]+num[1]}")
+  case '-':
+   print(f"The Substraction of Two Numbers is:{num[0]-num[1]}")
+  case '*':
+   print(f"The Multiplication of Two Numbers is:{num[0]*num[1]}")
+  case '/':
+   if(num[1]==0):
+    print("Division by Zero is not possible")
+   else:
+    print(f"The Division of Two Numbers is:{num[0]/num[1]}")
+  case '%':
+   if(num[1]==0):
+    print("Moduls by Zero is not possible")
+   else:
+    print(f"The Moduls of Two Numbers is:{num[0]%num[1]}")
+  case '//':
+   if(num[1]==0):
+    print("Floor Division by Zero is not possible")
+   else:
+    print(f"The Floor Division of Two Numbers is:{num[0]//num[1]}")
+  case '**':
+   print(f"The Exponetial of Two Numbers is:{num[0]**num[1]}")
