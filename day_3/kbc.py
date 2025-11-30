77042ec37d12c88687d228b7277b510b21ba3f8c
Mukul <mukulkhandalkar4@gmail.com>
2025-11-30 15:04:23 +0530

    Finished developing kbc game

--- day_3/kbc.py
+++ day_3/kbc.py
@@ -1,5 +1,5 @@
 print("********************  Welcome To KBC  ********************")
-print("Here is a chance to WIN BIG amount of 15000/- Rs.")
+print("Here is a chance to WIN BIG amount of 15,000/- Rs.")
 print("Simply answer 15 easy Questions to win the Prize amount")
 play_tuple=('yes','no')
 while True:
@@ -20,7 +20,7 @@ elif play=="yes":
 "Q2. Function to display output: A. echo() B. output() C. print() D. display()",
 "Q3. Valid variable name: A. 2name B. my-name C. my_name D. my name",
 "Q4. Data type for True/False: A. String B. Boolean C. Integer D. Float",
-"Q5. Symbol for comments in Python: A. // B. <!-- C. "#" D. /*",
+"Q5. Symbol for comments in Python: A. // B. <!-- C. '#' D. /* ",
 "Q6. Output of len('Python'): A. 5 B. 6 C. 7 D. 0",
 "Q7. Which is a list? A. {1,2,3} B. (1,2,3) C. [1,2,3] D. <1,2,3>",
 "Q8. Keyword to define a function: A. func B. define C. def D. function",
@@ -33,18 +33,22 @@ elif play=="yes":
 "Q15. Output of 2 + 3 * 4: A. 20 B. 14 C. 24 D. 18"
 ]
  al=["b","c","c","b","c","b","c","c","c","b","a","b","d","b","b"]
- i=PRIZE=0
- while i<15:
-  print(ql[i])
-  ans=input("Enter Your Desired Option from 'A , B , C , D':").lower()
-  if(ans==al[i]):
-   print("Congratulations...Keep Up The Winning Streak")
+ i=0
+ PRIZE=0
+ while True:
+  if(i<15):
+   print(ql[i])
+   ans=input("Enter Your Desired Option from 'A , B , C , D':").lower()
+  if(i==15):
+   print(f"Your winning Ammount is:{PRIZE}")
+   break
+  elif(ans==al[i]):
    i+=1
    PRIZE+=1000
+   if(i<14):
+    print("Congratulations...Keep Up The Winning Streak")
+   else:
+    print("Congratulations...You have answered all the Question Correctly")
   else:
    print("Sorry but that is a WRONG ANSWER...Better Luck Next Time...!!!")
    print(f"Your winning Prize is:{PRIZE}")
-   break
- if i==15:
-  print("Congratulations...You have answered all the Question Correctly")
-  print(f"Your winning Ammount is:{PRIZE}")
